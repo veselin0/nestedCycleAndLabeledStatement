8388a91eaecfc596b6b67fc404562878919ae206
Vesko <vesselin60@gmail.com>
2025-08-31 11:10:03 +0300

    Asterix Pyramide

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ for (let i = 0; i < 3; i++) {
         console.log(j);
     }
 }
+
+/*Asterix Pyramide
+*
+**
+***
+****
+*****
+******
+*/
+
+let result = '';
+const length = 7;
+
+for(let i = 1; i < length; i++){
+    for(let j = 0; j < i; j++){
+        result += '*';
+    }
+
+    result += '\n';
+}
+
+console.log(result);
