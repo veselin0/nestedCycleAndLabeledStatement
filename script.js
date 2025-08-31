222fb408f7d2c1fa7925736248ef4fef6d3b0692
Vesko <vesselin60@gmail.com>
2025-08-31 15:40:18 +0300

    Labeled Statement

--- script.js
+++ script.js
@@ -6,23 +6,36 @@ for (let i = 0; i < 3; i++) {
 }
 
 /*Asterix Pyramide
-*
-**
-***
-****
-*****
-******
-*/
+ *
+ **
+ ***
+ ****
+ *****
+ ******
+ */
 
-let result = '';
+let result = "";
 const length = 7;
 
-for(let i = 1; i < length; i++){
-    for(let j = 0; j < i; j++){
-        result += '*';
+for (let i = 1; i < length; i++) {
+    for (let j = 0; j < i; j++) {
+        result += "*";
     }
 
-    result += '\n';
+    result += "\n";
 }
 
 console.log(result);
+
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 5; k++) {
+            if(k === 2) continue first; // here we can use break, too, to stop the cycle
+            console.log(`Third level: ${k}`);
+        }
+    }
+
+}
